03dab99a108a4154949ee2e6780c784021430926
ksks723 <ksks723@gmail.com>
2020-05-26 00:17:33 +0900

    [슬기 back작업] 로그인, 로그아웃, 회원가입 분리

--- McDonalds/src/kr/or/mc/user/controller/UserBoardController.java
+++ McDonalds/src/kr/or/mc/user/controller/UserBoardController.java
@@ -2,6 +2,8 @@ package kr.or.mc.user.controller;
 
 
 import java.io.IOException;
+
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,16 +15,13 @@ import kr.or.mc.admin.service.member.MemberListService;
 import kr.or.mc.admin.service.member.MemberUpdateService;
 import kr.or.mc.common.action.Action;
 import kr.or.mc.common.action.ActionForward;
-import kr.or.mc.user.service.LoginService;
-import kr.or.mc.user.service.LogoutService;
-import kr.or.mc.user.service.NoticeListService;
-import kr.or.mc.user.service.RegisterService;
+import kr.or.mc.user.service.board.NoticeListService;
 
 @WebServlet("*.b")
 public class UserBoardController extends HttpServlet {
        
     public UserBoardController() {
-        super();
+        super(); 
         
     }
     
@@ -47,29 +46,7 @@ public class UserBoardController extends HttpServlet {
 			forward = new ActionForward();
 			forward.setPath("/WEB-INF/admin/Main.jsp");
 			
-		}else if (url_Command.equals("/login.b")) { // 회원리스트 페이지 이동 + 회원 수정 로직
-	         System.out.println("여기는 타는거구나?");
-	          action = new LoginService();
-	          forward = action.execute(request, response);
-	          
-	      }else if(url_Command.equals("/joinformOk.b")){ // 회원리스트 페이지 이동 + 회원 등록 로직
-	    	  System.out.println("JOINOK여기는 타는거구나?");
-	         action = new RegisterService();
-	         forward = action.execute(request, response);
-	         
-	      }else if(url_Command.equals("/logout.b")) { // 
-	         System.out.println("여기는 타는거구나?");
-	         action = new LogoutService();
-	         forward = action.execute(request, response);
-	      }else if(url_Command.equals("/Mcdonald_joinform.b")) { //회원가입화면 이동
-	    	  System.out.println("JoinJOIN");
-	    	  forward = new ActionForward();
-	    	  forward.setPath("/WEB-INF/user/Mcdonald_joinform.jsp");
-	      }else if(url_Command.contentEquals("/Mcdonald_login.b")){ //로그인화면 이동
-	    	  forward = new ActionForward();
-	    	  forward.setPath("/WEB-INF/user/Mcdonald_login.jsp");
-	      }
-		
+		}
 		if (forward != null) {
 			RequestDispatcher dis = request.getRequestDispatcher(forward.getPath());
 			dis.forward(request, response);
--- McDonalds/src/kr/or/mc/user/controller/UserMyPageController.java
+++ McDonalds/src/kr/or/mc/user/controller/UserMyPageController.java
@@ -1,41 +1,75 @@
 package kr.or.mc.user.controller;
 
 import java.io.IOException;
+
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FrontMyPageController
- */
-@WebServlet("/FrontMyPageController")
+import kr.or.mc.common.action.Action;
+import kr.or.mc.common.action.ActionForward;
+import kr.or.mc.user.service.mypage.LoginService;
+import kr.or.mc.user.service.mypage.LogoutService;
+import kr.or.mc.user.service.mypage.RegisterService;
+
+@WebServlet("*.ump")
 public class UserMyPageController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+  
     public UserMyPageController() {
         super();
-        // TODO Auto-generated constructor stub
     }
+    private void doProcess(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    	String requestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String url_Command = requestURI.substring(contextPath.length());
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+		Action action = null;
+		ActionForward forward = null; 
+		System.out.println("여긴오는데 왜 다음으로 안가"); 
+		
+		System.out.println(" url_Command : " + url_Command);
+			
+		if (url_Command.equals("/login.ump")) { // 공지사항 목록 뿌려주는 로직
+			System.out.println("여기는 타는거구나?");
+	          action = new LoginService();
+	          forward = action.execute(request, response);
+			
+		}else if(url_Command.equals("/joinformOk.ump")){ // 회원리스트 페이지 이동 + 회원 등록 로직
+	    	  System.out.println("JOINOK여기는 타는거구나?");
+		         action = new RegisterService();
+		         forward = action.execute(request, response);
+		         
+		      }else if(url_Command.equals("/logout.ump")) { // 
+		         System.out.println("여기는 타는거구나?");
+		         action = new LogoutService();
+		         forward = action.execute(request, response);
+		      
+		      }else if(url_Command.equals("/Mcdonald_joinform.ump")) { //회원가입화면 이동
+		    	  System.out.println("JoinJOIN");
+		    	  forward = new ActionForward();
+		    	  forward.setPath("/WEB-INF/user/Mcdonald_joinform.jsp");
+		     
+		      }else if(url_Command.contentEquals("/Mcdonald_login.ump")){ //로그인화면 이동
+		    	  forward = new ActionForward();
+		    	  forward.setPath("/WEB-INF/user/Mcdonald_login.jsp");
+		      }
+    	
+    	if (forward != null) {
+			RequestDispatcher dis = request.getRequestDispatcher(forward.getPath());
+			dis.forward(request, response);
+		}
+    }
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doProcess(request, response); 
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		doProcess(request, response); 
 	}
 
 }
